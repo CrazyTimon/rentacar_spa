49b021cf1cd445e7e6191df13498f97f1e12a7ea
Timur Munykin <timur.munykin@team.wrike.com>
2017-09-26 02:52:13 +0300

    stage 1

--- lib/client/components/root_component.dart
+++ lib/client/components/root_component.dart
@@ -1,20 +1,21 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
-// import 'package:rentacar_spa/client/components/car/car.dart';
+import 'package:rentacar_spa/client/components/car/car.dart';
+import 'package:rentacar_spa/client/components/left_menu/left_menu.dart';
 
 
 @Component(
   selector: 'my-app',
   templateUrl: 'root_component.html',
   directives: const [
-    // CarComponent
+    CarComponent,
     DeferredContentDirective,
     MaterialButtonComponent,
     MaterialIconComponent,
     MaterialPersistentDrawerDirective,
     MaterialToggleComponent,
     MaterialListComponent,
-    MaterialListItemComponent
+    LeftMenu
   ],
   styleUrls: const [
     'package:angular_components/src/components/app_layout/layout.scss.css',
@@ -25,5 +26,13 @@ import 'package:angular_components/angular_components.dart';
   ]
 )
 class AppComponent {
-   bool end = false;
+  bool end = false;
+
+  // CarManager _carManager;
+  // ClassificationManager _clsManager;
+  // GearboxManager _gearboxManager;
+
+  // AppComponent(this._carManager) {
+  //   _carManager.getAll();
+  // }
 }
